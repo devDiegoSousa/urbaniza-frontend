105d4481f709ff49424938403dd916eba5cbbdad
devDiego <diego.carvalho10@fatec.sp.gov.br>
2025-05-03 11:59:32 -0300

    chore: finalize page layout structure
    This commit completes the structure for the common layout that will be used across all pages in the project. It sets the foundation for consistent styling, ensuring all pages follow the same structural guidelines, including header, footer, and navigation placement. No new features have been added; the focus is on providing a unified skeleton for the entire application.

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{html}",
+    "./src/script/global/**/*.{js}",
+    "./src/script/pages/**/*.{js}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'dark-black': '#121212',
+      },
+    },
+  },
+  plugins: [],
+}
